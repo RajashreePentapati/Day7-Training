26187e5e43e1fa29bafd58c84c072962e454fd55
RajashreePentapati <143689602+RajashreePentapati@users.noreply.github.com>
2024-07-30 12:38:44 -0400

    simple.py

--- simple.py
+++ simple.py
@@ -1,9 +1,5 @@
-#!/usr/bin/env python
+# helloworld/simple.py
 
-"""Top-level script to invoke helloworld implementation."""
-
-import sys
-import helloworld.main
-
-if __name__ == '__main__':
-    sys.exit(helloworld.main.main())
+def main():
+    print("Hello Rajashree")
+    return 0
